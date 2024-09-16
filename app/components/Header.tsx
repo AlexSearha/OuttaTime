79176c3d8ef761743a5efe6144fe7545e20fdaf8
AlexSearha <contact@mytechcompanion.fr>
2024-09-16 17:27:08 +0200

    firest header & footer

---
+++ app/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavMenu from "./NavMenu";
+
+const Header = () => {
+  return (
+    <header className="h-20 bg-red-300">
+      <NavMenu />
+    </header>
+  );
+};
+
+export default Header;
