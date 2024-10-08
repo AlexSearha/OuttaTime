dd307ad9483054c43361e4ebbdce264e7cc663a1
AlexSearha <contact@mytechcompanion.fr>
2024-10-08 21:51:58 +0200

    add calculat teams choice by players

--- app/jouer/page.tsx
+++ app/jouer/page.tsx
@@ -1,5 +1,71 @@
-import TimeFunction from "../components/TimeFunction"
+"use client"
+import { FormControl, FormGroup, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material"
+import { useState } from "react"
 
 export default function Jouer() {
-    return <TimeFunction />
+    const [playersNumber, setPlayersNumber] = useState("")
+    const [teams, setTeams] = useState("")
+
+    const handleChange = (event: SelectChangeEvent<string>) => {
+        setPlayersNumber(event.target.value)
+    }
+
+    const handleTeamsChange = (event: SelectChangeEvent<string>) => {
+        setTeams(event.target.value)
+    }
+
+    const calculateTeams = (playersNumber: number) => {
+        const possibleTeams = []
+        for (let i = 2; i <= Math.floor(playersNumber / 2); i++) {
+            if (playersNumber % i === 0) {
+                possibleTeams.push(i)
+            }
+        }
+        return possibleTeams
+    }
+
+    return (
+        <>
+            <FormGroup className="flex flex-col gap-2">
+                <FormControl fullWidth>
+                    <InputLabel htmlFor="players-number">Nombre de joueurs</InputLabel>
+                    <Select
+                        labelId="players-number"
+                        id="players-number"
+                        value={playersNumber}
+                        label="Nombre de joueurs"
+                        onChange={(event) => handleChange(event)}
+                    >
+                        {/* Générer uniquement des nombres pairs */}
+                        {Array.from({ length: 6 }, (_, i) => (i + 1) * 2).map((value) => (
+                            <MenuItem key={value} value={value}>
+                                {value}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+
+                {playersNumber !== "" && (
+                    <>
+                        <FormControl fullWidth>
+                            <InputLabel htmlFor="teams-number">Nombre d&apos;équipes</InputLabel>
+                            <Select
+                                labelId="teams-number"
+                                id="teams-number"
+                                value={teams}
+                                label="Nombre d'équipes"
+                                onChange={(event) => handleTeamsChange(event)}
+                            >
+                                {calculateTeams(Number(playersNumber)).map((teamCount) => (
+                                    <MenuItem key={teamCount} value={teamCount}>
+                                        {teamCount}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </>
+                )}
+            </FormGroup>
+        </>
+    )
 }
